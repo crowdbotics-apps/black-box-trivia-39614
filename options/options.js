1dcb8fd6874870b70f79d1545f3df5b7e9cd513f
Crowdbotics <team@crowdbotics.com>
2023-03-30 01:38:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "black-box-trivia-39614",
+  url: "https://black-box-trivia-39614.botics.co",
+  api: "https://black-box-trivia-39614.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
